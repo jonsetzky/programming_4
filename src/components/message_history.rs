175ac9bb1fba3cc8558324af7cb5662647e833d7
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2026-02-10 15:10:30 +0200

    add combining messages from the same sender

--- src/components/message_history.rs
+++ src/components/message_history.rs
@@ -1,13 +1,18 @@
-use chrono::{DateTime, Local, Timelike};
+use chrono::{DateTime, Datelike, Local, Timelike};
 use dioxus::prelude::*;
 
 use crate::{AppState, packet::ChatMessage};
 
 #[component]
-fn Message(message: ChatMessage, is_me: bool) -> Element {
-    let time = message.datetime().unwrap();
+fn Message(message: UIChatMessage, is_me: bool) -> Element {
+    let time = message.message.datetime().unwrap();
     let time: DateTime<Local> = time.into();
     let time = format!("{:02}:{:02}", time.hour(), time.minute());
+
+    let content = message.message.message;
+    let user = message.message.user;
+    let show_user = message.show_user;
+    let show_time = message.show_time;
 
     rsx! {
         div {
@@ -17,16 +22,20 @@ fn Message(message: ChatMessage, is_me: bool) -> Element {
             font_size: "12px",
             overflow_wrap: "break-word",
             justify_items: if is_me { "end" } else { "start" },
-            p { margin: "8px 0px 2px 0px", "{message.user}" }
+            if show_user {
+                p { margin: "8px 0px 2px 0px", "{user}" }
+            }
             div {
                 max_width: "29rem",
                 font_size: "12px",
                 background_color: "#262626",
                 border_radius: "6px",
                 padding: "8px 10px 10px 10px",
-                p { user_select: "text", "{message.message}" }
+                p { user_select: "text", "{content}" }
             }
-            p { margin: "2px 0px 0px 0px", color: "#727272", "{time}" }
+            if show_time {
+                p { margin: "2px 0px 0px 0px", color: "#727272", "{time}" }
+            }
         }
     }
 }
@@ -62,23 +71,68 @@ async fn should_autoscroll() -> Option<bool> {
     Some(out < AUTOSCROLL_THRESHOLD)
 }
 
+#[derive(Clone, PartialEq)]
+struct UIChatMessage {
+    message: ChatMessage,
+    show_user: bool,
+    show_time: bool,
+}
+
+fn combine_messages(messages: Vec<ChatMessage>) -> Vec<UIChatMessage> {
+    if messages.is_empty() {
+        return Vec::<UIChatMessage>::default();
+    }
+
+    let mut out = Vec::<UIChatMessage>::new();
+
+    let mut prev: Option<&mut UIChatMessage> = None;
+    let mut iter = messages.iter();
+
+    while let Some(current) = iter.next() {
+        let mut show_user = true;
+        if let Some(prev) = prev {
+            let cd = current.datetime().unwrap();
+            let pt = prev.message.datetime().unwrap();
+
+            // not perfect logic but should suffice
+            let is_same_time =
+                cd.minute() == pt.minute() && cd.hour() == pt.hour() && cd.day() == pt.day();
+
+            if is_same_time && current.user == prev.message.user {
+                prev.show_time = false;
+                show_user = false;
+            }
+        }
+
+        out.push(UIChatMessage {
+            message: current.clone(),
+            show_user: show_user,
+            show_time: true,
+        });
+
+        prev = out.last_mut();
+    }
+    out
+}
+
 #[component]
 pub fn MessageHistory(messages: Memo<Vec<ChatMessage>>) -> Element {
     let state = use_context::<AppState>();
     let username = state.username;
 
+    let mut final_messages = use_signal(|| Vec::<UIChatMessage>::new());
+
     use_effect(move || {
-        // run this effect every time messages update
-        messages();
+        final_messages.set(combine_messages(messages()));
 
         spawn(async move {
             if should_autoscroll().await.unwrap_or(false) {
                 scroll_to_anchor();
             }
         });
-    });
 
-    // todo join continuous messages from same sender during same minute into one block
+        println!("updating messages");
+    });
 
     rsx! {
         div {
@@ -93,10 +147,10 @@ pub fn MessageHistory(messages: Memo<Vec<ChatMessage>>) -> Element {
             justify_content: "flex-start",
             align_items: "center",
             padding: "0px 100px auto 0px",
-            for message in messages().iter() {
+            for message in final_messages.read().iter() {
                 Message {
                     message: message.clone(),
-                    is_me: message.user == username(),
+                    is_me: message.message.user == username(),
                 }
             }
 
